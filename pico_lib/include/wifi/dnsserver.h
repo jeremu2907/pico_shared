69d87a4b5abd16bc7296cf451a65419f595924fd
jeremu2907 <jeremu2907@gmail.com>
2025-12-19 01:17:05 -0600

    WIP

---
+++ pico_lib/include/wifi/dnsserver.h
@@ -0,0 +1,14 @@
+#ifndef DNS_SERVER_H
+#define DNS_SERVER_H
+
+#include "lwip/ip_addr.h"
+
+typedef struct dns_server_t_ {
+    struct udp_pcb *udp;
+     ip_addr_t ip;
+} dns_server_t;
+
+void dns_server_init(dns_server_t *d, ip_addr_t *ip);
+void dns_server_deinit(dns_server_t *d);
+
+#endif
