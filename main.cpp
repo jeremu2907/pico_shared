5954e3a382a2558282f8db90c67110bea9aa55c0
jeremu2907 <jeremu2907@gmail.com>
2025-12-18 21:15:36 -0600

    wip

--- main.cpp
+++ main.cpp
@@ -1,27 +1,32 @@
+#include <stdio.h>
+
 #include "pico/stdlib.h"
 #include "pico/cyw43_arch.h"
 
-#include "Macros.hpp"
+const char SSID[] = "PICO-AP";
+const char PASSWORD[] = "p@ssw0rd";
 
-int main()
-{
+int main() {
     stdio_init_all();
+    sleep_ms(1000);
 
-    if(cyw43_arch_init())
-    {
-        ERR_START
-        printf("CYW43 init failed\n");
-        ERR_END
+    printf("Initializng CYW34...");
+    if (cyw43_arch_init()) {
+        return 1;
     }
-    
-    while (true)
-    {
-        printf("LED ON\n");
-        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
-        sleep_ms(500);
-        
-        printf("LED OFF\n");
-        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
-        sleep_ms(500);
+    printf("CYW34 init done...\n");
+
+    cyw43_wifi_ap_set_channel(&cyw43_state, 6);
+
+    cyw43_arch_enable_ap_mode(
+        SSID,
+        PASSWORD,
+        CYW43_AUTH_WPA2_AES_PSK
+    );
+
+    printf("AP started\n");
+    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+
+    while (true) {
     }
 }
